44ae72b2133daf6e437f534b7ebe57b1d8ad206e
hr645364 <128062621+hr645364@users.noreply.github.com>
2023-05-29 01:46:56 -0700

    completed

---
+++ public/app.js
@@ -0,0 +1,11 @@
+var div =document.getElementById('div');
+
+function circle() {
+    div.className = 'circle';
+}
+function rectangular() {
+    div.className = 'rectangular';
+}
+function square() {
+    div.className = 'square';
+}
